eaf3ec03728590aeb9a63d98fa20180a9e4a19f1
IdeaPad <IdeaPad@DESKTOP-3LJMEES>
2025-02-20 11:37:43 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+    int a = 2, b = 3;
+    Console.WriteLine($"Резултат: {Power(a, b)}");
+}
+
+static int Power(int a, int b)
+{
+    if (b == 0) return 1; 
+    return a * Power(a, b - 1); 
+}
